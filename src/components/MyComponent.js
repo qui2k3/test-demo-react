f28919450ce8aca4b57f639058153d453c19f279
qui2k3 <6251071074@st.utc2.edu.vn>
2024-05-18 00:00:44 +0700

    form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,19 +7,36 @@ class MyComponent extends React.Component {
     };
 
     handleOnClick(event){
-        console.log(event)
         console.log(`Toi ten la ${this.state.name}`)
+        this.setState({
+            name:"ProVjp",
+            age: Math.floor(Math.random() * 10)+1
+        })
     }
-    handleOnMouseOver(event){
+    handleOnMouseOver=(event)=>{
         console.log(event)
+    }
+    handleOnChange=(event)=>{
+        this.setState({
+            name:event.target.value
+        })
+    }
+    handleOnSubmit=(event)=>{
+        event.preventDefault();
+        console.log(this.state)
     }
     //JSX
     render(){
-        return(
+        return( 
             <div>
                My name is {this.state.name} and I'm from {this.state.address}, I'm {this.state.age} year old
-               <button onClick={this.handleOnClick}>Click Me!</button>
-               <button onMouseOver={this.handleOnMouseOver}>Hover Me!</button>
+               
+               <form onSubmit={(event)=>{this.handleOnSubmit(event)}}>
+                <input type="text"
+                onChange={(event)=>{this.handleOnChange(event)}}
+                />
+                <button>Submit</button>
+               </form>
             </div>
         );
     }
