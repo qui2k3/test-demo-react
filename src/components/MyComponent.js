57343bbb6c793e7a069ce32767aee7d69f952a0a
qui2k3 <6251071074@st.utc2.edu.vn>
2024-05-18 22:41:06 +0700

    tach code

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,42 +1,11 @@
 import React from "react";
+import UserInfor from "./UserInfo";
 class MyComponent extends React.Component {
-    state ={
-        name: "Qui",
-        address:"Tien Giang",
-        age:21
-    };
-
-    handleOnClick(event){
-        console.log(`Toi ten la ${this.state.name}`)
-        this.setState({
-            name:"ProVjp",
-            age: Math.floor(Math.random() * 10)+1
-        })
-    }
-    handleOnMouseOver=(event)=>{
-        console.log(event)
-    }
-    handleOnChange=(event)=>{
-        this.setState({
-            name:event.target.value
-        })
-    }
-    handleOnSubmit=(event)=>{
-        event.preventDefault();
-        console.log(this.state)
-    }
     //JSX
-    render(){
-        return( 
+    render() {
+        return (
             <div>
-               My name is {this.state.name} and I'm from {this.state.address}, I'm {this.state.age} year old
-               
-               <form onSubmit={(event)=>{this.handleOnSubmit(event)}}>
-                <input type="text"
-                onChange={(event)=>{this.handleOnChange(event)}}
-                />
-                <button>Submit</button>
-               </form>
+                <UserInfor></UserInfor>
             </div>
         );
     }
