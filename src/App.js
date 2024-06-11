10461648810237b764b79aaa793ee86239fa36ec
qui2k3 <6251071074@st.utc2.edu.vn>
2024-06-11 20:54:32 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { Button } from 'react-bootstrap';
 import './App.scss';
+import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
+
 
 
 
@@ -6,7 +10,14 @@ const App = () => {
 
   return (
     <div className="app-container">
-      hello world
+      <Header />
+      <div>
+        test link
+        <div>
+          <button ><Link to="/users">Go to user page</Link></button>
+          <button ><Link to="/admins">Go to admin page</Link></button>
+        </div>
+      </div>
     </div>
   );
 }
